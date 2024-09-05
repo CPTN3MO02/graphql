14e45046d1fb43d8e30168e742bbdd749d1ef679
CPTN3MO02 <101760040+CPTN3MO02@users.noreply.github.com>
2024-09-05 17:00:58 +0000

    Add files via upload

---
+++ graphql/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware pour servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'web')));
+
+// Route pour le fichie r login.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'web', 'login.html'));
+});
+
+// Route pour le fichier index.html
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'web', 'index.html'));
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`);
+});
